c1d51747727854462c22fd1ffc36a3623d20211b
juancoob <ja.cobos.obrero@gmail.com>
2018-07-27 17:24:05 +0200

    feat: Added NetworkState file to check the network state

---
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/util/NetworkState.java
@@ -0,0 +1,39 @@
+package com.juancoob.nanodegree.and.vegginner.util;
+
+/**
+ * Created by Juan Antonio Cobos Obrero on 25/07/18.
+ */
+public class NetworkState {
+
+    public static final NetworkState LOADED;
+    public static final NetworkState LOADING;
+
+    static {
+        LOADED = new NetworkState(Status.SUCCESS, Constants.SUCCESS);
+        LOADING = new NetworkState(Status.RUNNING, Constants.RUNNING);
+    }
+
+    private final Status state;
+    private final String msg;
+
+    public NetworkState(Status state, String msg) {
+        this.state = state;
+        this.msg = msg;
+    }
+
+    public Status getState() {
+        return state;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public enum Status {
+        RUNNING,
+        SUCCESS,
+        FAILED,
+        NO_INTERNET
+    }
+
+}
