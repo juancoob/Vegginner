b56ba0a6c9fe4208eedbe799f2663c1bd3155825
juancoob <ja.cobos.obrero@gmail.com>
2018-07-27 21:13:34 +0200

    feat: Modified the Application to get its instance and the Recipes Api instance

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/VegginnerApp.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/VegginnerApp.java
@@ -1,8 +1,11 @@
 package com.juancoob.nanodegree.and.vegginner;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
+import com.juancoob.nanodegree.and.vegginner.data.RestApiFactory;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.remote.IRecipeApiService;
 import com.juancoob.nanodegree.and.vegginner.util.timber.TimberLog;
 
 import io.fabric.sdk.android.Fabric;
@@ -11,6 +14,10 @@ import io.fabric.sdk.android.Fabric;
  * Created by Juan Antonio Cobos Obrero on 21/07/18.
  */
 public class VegginnerApp extends Application{
+
+    //todo dagger 2
+    public IRecipeApiService recipeApiService;
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -20,5 +27,18 @@ public class VegginnerApp extends Application{
                 .debuggable(true) // Enables Crashlytics debugger
                 .build();
         Fabric.with(fabric);
+    }
+
+    //todo dagger 2
+    public static VegginnerApp get(Context ctx) {
+        return (VegginnerApp) ctx.getApplicationContext();
+    }
+
+    //todo pasarlo a dagger 2
+    public IRecipeApiService getRecipeRestApi() {
+        if(recipeApiService == null) {
+            recipeApiService = RestApiFactory.createRecipeApi();
+        }
+        return recipeApiService;
     }
 }
