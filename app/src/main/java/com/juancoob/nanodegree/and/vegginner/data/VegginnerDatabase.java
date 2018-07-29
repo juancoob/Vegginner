b0f9f60d16e7ce628db1a1977eaab9949658ef59
juancoob <ja.cobos.obrero@gmail.com>
2018-07-29 22:04:15 +0200

    refactor: Updated database name

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/VegginnerDatabase.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/VegginnerDatabase.java
@@ -1,13 +1,16 @@
-package com.juancoob.nanodegree.and.vegginner.data.recipes.local;
+package com.juancoob.nanodegree.and.vegginner.data;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
+
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipe;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipeDao;
 
 /**
  * Created by Juan Antonio Cobos Obrero on 25/07/18.
  */
 
 @Database(entities = {FavoriteRecipe.class}, version = 1, exportSchema = false)
-public abstract class FavoriteRecipeDatabase extends RoomDatabase {
+public abstract class VegginnerDatabase extends RoomDatabase {
     public abstract FavoriteRecipeDao favoriteRecipeDao();
 }
