4d2c78defe7d0ccb7ce0a23473ea93af9057a8bb
juancoob <ja.cobos.obrero@gmail.com>
2018-07-30 23:15:41 +0200

    feat: Updated the MainActivity to communicate with the ViewModel to create a new fragment

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/ui/MainActivity.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.juancoob.nanodegree.and.vegginner.ui;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -14,12 +15,20 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.juancoob.nanodegree.and.vegginner.R;
+import com.juancoob.nanodegree.and.vegginner.VegginnerApp;
 import com.juancoob.nanodegree.and.vegginner.ui.beginning.BeginningFragment;
+import com.juancoob.nanodegree.and.vegginner.ui.recipeDetails.RecipeDetailsFragment;
 import com.juancoob.nanodegree.and.vegginner.ui.recipes.RecipesFragment;
+import com.juancoob.nanodegree.and.vegginner.util.Constants;
+import com.juancoob.nanodegree.and.vegginner.viewmodel.RecipesViewModel;
+import com.juancoob.nanodegree.and.vegginner.viewmodel.VegginnerViewModelFactory;
+
+import javax.inject.Inject;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,14 +47,30 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @BindView(R.id.nv_main)
     public NavigationView mainNavigationView;
 
+    @Inject
+    public VegginnerViewModelFactory vegginnerViewModelFactory;
+
+    private boolean mBackToMainWhenPressBack;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ((VegginnerApp) getApplication()).getRecipeComponent().injectMainActivity(this);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         setToolbar();
         mainNavigationView.setNavigationItemSelectedListener(this);
         setFragment();
+        RecipesViewModel recipesViewModel = ViewModelProviders.of(this, vegginnerViewModelFactory).get(RecipesViewModel.class);
+        recipesViewModel.getFragmentDetailToReplace().observe(this, fragmentDetailToReplace -> {
+            mBackToMainWhenPressBack = true;
+            if (Constants.RECIPE_DETAILS.equals(fragmentDetailToReplace)) {
+                removeFragmentFromTheStack();
+                replaceFragmentToActivity(getSupportFragmentManager(), RecipeDetailsFragment.getInstance(), R.id.fl_main_content, true);
+            }
+            hideToolbar();
+            lockDrawer();
+        });
     }
 
     private void setToolbar() {
@@ -60,14 +85,15 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void setFragment() {
         FragmentManager manager = getSupportFragmentManager();
-        if(manager.findFragmentByTag("") == null) {
-            replaceFragmentToActivity(getSupportFragmentManager(), BeginningFragment.getInstance(), R.id.fl_main_content);
+        if (manager.findFragmentByTag("") == null) {
+            replaceFragmentToActivity(getSupportFragmentManager(), BeginningFragment.getInstance(), R.id.fl_main_content, false);
         }
     }
 
-    private void replaceFragmentToActivity(FragmentManager fragmentManager, Fragment fragment, int fragmentId) {
+    private void replaceFragmentToActivity(FragmentManager fragmentManager, Fragment fragment, int fragmentId, boolean addToBackStack) {
         FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(fragmentId, fragment, "");
+        if (addToBackStack) transaction.addToBackStack(null);
         transaction.commit();
     }
 
@@ -75,7 +101,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                mainDrawerLayout.openDrawer(GravityCompat.START);
+                if (mBackToMainWhenPressBack) {
+                    onBackPressed();
+                } else {
+                    mainDrawerLayout.openDrawer(GravityCompat.START);
+                }
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -85,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.nav_beginning:
-                replaceFragmentToActivity(getSupportFragmentManager(), BeginningFragment.getInstance(), R.id.fl_main_content);
+                replaceFragmentToActivity(getSupportFragmentManager(), BeginningFragment.getInstance(), R.id.fl_main_content, false);
                 break;
             case R.id.nav_advices:
                 Toast.makeText(MainActivity.this, "Advices", Toast.LENGTH_SHORT).show();
@@ -94,7 +124,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 Toast.makeText(MainActivity.this, "Equivalencies", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.nav_recipes:
-                replaceFragmentToActivity(getSupportFragmentManager(), RecipesFragment.getInstance(), R.id.fl_main_content);
+                replaceFragmentToActivity(getSupportFragmentManager(), RecipesFragment.getInstance(), R.id.fl_main_content, false);
                 break;
             case R.id.nav_places:
                 Toast.makeText(MainActivity.this, "Places", Toast.LENGTH_SHORT).show();
@@ -113,6 +143,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     public void onBackPressed() {
         if (mainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
             mainDrawerLayout.closeDrawers();
+        } else if (mBackToMainWhenPressBack) {
+            removeFragmentFromTheStack();
+            mBackToMainWhenPressBack = false;
+            showToolbar();
+            setToolbar();
+            unlockDrawer();
         } else {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(R.string.exit_dialog_title)
@@ -131,5 +167,25 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                         }
                     }).show();
         }
+    }
+
+    private void hideToolbar() {
+        mainToolbar.setVisibility(View.GONE);
+    }
+
+    private void showToolbar() {
+        mainToolbar.setVisibility(View.VISIBLE);
+    }
+
+    private void lockDrawer() {
+        mainDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+    }
+
+    private void unlockDrawer() {
+        mainDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+    }
+
+    private void removeFragmentFromTheStack() {
+        getSupportFragmentManager().popBackStack();
     }
 }
