3e29fe9e4fec51d28f9b13f288b3d741f2b13614
juancoob <ja.cobos.obrero@gmail.com>
2018-08-03 23:57:44 +0200

    feat: Updated database with the ingredient table

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/VegginnerDatabase.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/VegginnerDatabase.java
@@ -3,14 +3,19 @@ package com.juancoob.nanodegree.and.vegginner.data;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipe;
-import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipeDao;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.favoriteRecipe.FavoriteRecipe;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.favoriteRecipe.FavoriteRecipeDao;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.ingredient.Ingredient;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.ingredient.IngredientDao;
 
 /**
+ * This abstract class is the main database
+ *
  * Created by Juan Antonio Cobos Obrero on 25/07/18.
  */
 
-@Database(entities = {FavoriteRecipe.class}, version = 1, exportSchema = false)
+@Database(entities = {FavoriteRecipe.class, Ingredient.class}, version = 1, exportSchema = false)
 public abstract class VegginnerDatabase extends RoomDatabase {
     public abstract FavoriteRecipeDao favoriteRecipeDao();
+    public abstract IngredientDao ingredientDao();
 }
