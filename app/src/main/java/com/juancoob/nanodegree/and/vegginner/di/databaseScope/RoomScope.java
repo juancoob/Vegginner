9c7fbb9d26959d801e8f55c6f5d7927fbfe5973e
juancoob <ja.cobos.obrero@gmail.com>
2018-08-04 18:24:52 +0200

    refactor: Changed scope name because its scope is the Room database

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/RoomScope.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/RoomScope.java
@@ -6,9 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import javax.inject.Scope;
 
 /**
+ * This is the scope to use some providers
+ *
  * Created by Juan Antonio Cobos Obrero on 28/07/18.
  */
 @Scope
 @Retention(RetentionPolicy.CLASS)
-public @interface RecipeScope {
+public @interface RoomScope {
 }
