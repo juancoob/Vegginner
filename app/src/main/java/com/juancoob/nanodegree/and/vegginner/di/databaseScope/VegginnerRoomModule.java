e1ec0ce9fa6b85bd8dbdf7e085fc31f7b04d95e4
juancoob <ja.cobos.obrero@gmail.com>
2018-08-04 18:45:00 +0200

    feat: Updated VegginnerRoomModule to retrieve IngredientRepository, IngredientDao, FavoriteRepository, FavoriteDao and changed scope

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/VegginnerRoomModule.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/VegginnerRoomModule.java
@@ -5,10 +5,13 @@ import android.app.Application;
 import android.arch.lifecycle.ViewModelProvider;
 import android.arch.persistence.room.Room;
 
-import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipeDao;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.favoriteRecipe.FavoriteRecipeDao;
 import com.juancoob.nanodegree.and.vegginner.data.VegginnerDatabase;
-import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipeRepository;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.favoriteRecipe.FavoriteRecipeRepository;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.ingredient.IngredientDao;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.ingredient.IngredientRepository;
 import com.juancoob.nanodegree.and.vegginner.data.recipes.remote.IRecipeApiService;
+import com.juancoob.nanodegree.and.vegginner.util.AppExecutors;
 import com.juancoob.nanodegree.and.vegginner.viewmodel.VegginnerViewModelFactory;
 
 import javax.inject.Singleton;
@@ -17,6 +20,8 @@ import dagger.Module;
 import dagger.Provides;
 
 /**
+ * This is the Room module to get the Room and ViewModel factory instances
+ *
  * Created by Juan Antonio Cobos Obrero on 28/07/18.
  */
 @Module
@@ -31,24 +36,38 @@ public class VegginnerRoomModule {
 
     @Provides
     @Singleton
-    ViewModelProvider.Factory provideViewModelFactory(FavoriteRecipeRepository favoriteRecipeRepository, IRecipeApiService recipeApiService) {
-        return new VegginnerViewModelFactory(favoriteRecipeRepository, recipeApiService);
+    ViewModelProvider.Factory provideViewModelFactory(FavoriteRecipeRepository favoriteRecipeRepository,
+                                                      IngredientRepository ingredientRepository,
+                                                      IRecipeApiService recipeApiService) {
+        return new VegginnerViewModelFactory(favoriteRecipeRepository, ingredientRepository, recipeApiService);
     }
 
     @Provides
-    @RecipeScope
-    FavoriteRecipeRepository provideFavoriteRecipeRepository(FavoriteRecipeDao favoriteRecipeDao) {
-        return new FavoriteRecipeRepository(favoriteRecipeDao);
+    @RoomScope
+    IngredientRepository provideIngredientRepository(IngredientDao ingredientDao, AppExecutors appExecutors) {
+        return new IngredientRepository(ingredientDao, appExecutors);
     }
 
     @Provides
-    @RecipeScope
+    @RoomScope
+    IngredientDao provideIngredientDao(VegginnerDatabase vegginnerDatabase) {
+        return vegginnerDatabase.ingredientDao();
+    }
+
+    @Provides
+    @RoomScope
+    FavoriteRecipeRepository provideFavoriteRecipeRepository(FavoriteRecipeDao favoriteRecipeDao, AppExecutors appExecutors) {
+        return new FavoriteRecipeRepository(favoriteRecipeDao, appExecutors);
+    }
+
+    @Provides
+    @RoomScope
     FavoriteRecipeDao provideFavoriteRecipeDao(VegginnerDatabase vegginnerDatabase) {
         return vegginnerDatabase.favoriteRecipeDao();
     }
 
     @Provides
-    @RecipeScope
+    @RoomScope
     VegginnerDatabase provideFavoriteRecipeDatabase() {
         return mVegginnerDatabase;
     }
