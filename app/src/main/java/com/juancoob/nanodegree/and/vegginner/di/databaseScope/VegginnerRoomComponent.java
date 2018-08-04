b31926e91187c2ac5530033ee20da9f23192a6cc
juancoob <ja.cobos.obrero@gmail.com>
2018-08-04 18:42:47 +0200

    feat: Updated VegginnerRoomComponent to inject RecipeIngredientsServingsFragment

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/VegginnerRoomComponent.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/di/databaseScope/VegginnerRoomComponent.java
@@ -3,20 +3,24 @@ package com.juancoob.nanodegree.and.vegginner.di.databaseScope;
 import com.juancoob.nanodegree.and.vegginner.di.VegginnerAppComponent;
 import com.juancoob.nanodegree.and.vegginner.ui.MainActivity;
 import com.juancoob.nanodegree.and.vegginner.ui.recipeDetails.RecipeDetailsFragment;
+import com.juancoob.nanodegree.and.vegginner.ui.recipeDetails.RecipeIngredientsServingsFragment;
 import com.juancoob.nanodegree.and.vegginner.ui.recipes.RecipesFragment;
 
 import dagger.Component;
 
 /**
+ * This is the Room component which connects modules and depends on VegginnerAppComponent
+ *
  * Created by Juan Antonio Cobos Obrero on 28/07/18.
  */
 
-@RecipeScope
+@RoomScope
 @Component(modules = VegginnerRoomModule.class, dependencies = VegginnerAppComponent.class)
 public interface VegginnerRoomComponent {
 
     void injectRecipesSection(RecipesFragment recipesFragment);
     void injectRecipeDetailsSection(RecipeDetailsFragment recipeDetailsFragment);
+    void injectRecipeIngredientsServingsFragment(RecipeIngredientsServingsFragment recipeIngredientsServingsFragment);
     void injectMainActivity(MainActivity mainActivity);
 
 }
