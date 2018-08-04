76be6bad3035d1343a4a1fc5ad355e421dcea13c
juancoob <ja.cobos.obrero@gmail.com>
2018-08-04 19:11:58 +0200

    feat: Updated VegginnerViewModelFactory to add the ingredient repository to RecipesViewModel

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/viewmodel/VegginnerViewModelFactory.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/viewmodel/VegginnerViewModelFactory.java
@@ -4,22 +4,29 @@ import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
 
-import com.juancoob.nanodegree.and.vegginner.data.recipes.local.FavoriteRecipeRepository;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.favoriteRecipe.FavoriteRecipeRepository;
+import com.juancoob.nanodegree.and.vegginner.data.recipes.local.ingredient.IngredientRepository;
 import com.juancoob.nanodegree.and.vegginner.data.recipes.remote.IRecipeApiService;
 
 import javax.inject.Inject;
 
 /**
+ * This class is the ViewModel factory which gives the correct ViewModel for every class
+ *
  * Created by Juan Antonio Cobos Obrero on 29/07/18.
  */
 public class VegginnerViewModelFactory implements ViewModelProvider.Factory {
 
     private FavoriteRecipeRepository mFavoriteRecipeRepository;
+    private IngredientRepository mIngredientRepository;
     private IRecipeApiService mRecipeApiService;
 
     @Inject
-    public VegginnerViewModelFactory(FavoriteRecipeRepository favoriteRecipeRepository, IRecipeApiService recipeApiService) {
+    public VegginnerViewModelFactory(FavoriteRecipeRepository favoriteRecipeRepository,
+                                     IngredientRepository ingredientRepository,
+                                     IRecipeApiService recipeApiService) {
         mFavoriteRecipeRepository = favoriteRecipeRepository;
+        mIngredientRepository = ingredientRepository;
         mRecipeApiService = recipeApiService;
     }
 
@@ -28,7 +35,7 @@ public class VegginnerViewModelFactory implements ViewModelProvider.Factory {
     @SuppressWarnings("unchecked")
     public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         if(modelClass.isAssignableFrom(RecipesViewModel.class)) {
-            return (T) new RecipesViewModel(mFavoriteRecipeRepository, mRecipeApiService);
+            return (T) new RecipesViewModel(mFavoriteRecipeRepository, mIngredientRepository, mRecipeApiService);
         } else {
             throw new IllegalArgumentException("ViewModel " + modelClass + " not found");
         }
