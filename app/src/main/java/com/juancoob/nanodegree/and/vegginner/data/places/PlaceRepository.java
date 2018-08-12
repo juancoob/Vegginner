e1e7e278a047f2e67e669d04f3a5218b21f8fc30
juancoob <ja.cobos.obrero@gmail.com>
2018-08-12 16:10:50 +0200

    feat: Added PlaceRepository to store the selected place type

---
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/places/PlaceRepository.java
@@ -0,0 +1,19 @@
+package com.juancoob.nanodegree.and.vegginner.data.places;
+
+/**
+ * This repository stores the selected place type on places section
+ *
+ * Created by Juan Antonio Cobos Obrero on 10/08/18.
+ */
+public class PlaceRepository {
+
+    private String mPlaceType;
+
+    public String getPlaceType() {
+        return mPlaceType;
+    }
+
+    public void setPlaceType(String mPlaceType) {
+        this.mPlaceType = mPlaceType;
+    }
+}
