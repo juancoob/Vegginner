8905029cb5a50ba2ebba79a886098e60bc140679
juancoob <ja.cobos.obrero@gmail.com>
2018-09-24 10:40:19 +0200

    fix: Added the scrolling functionality to the main text

--- app/src/main/java/com/juancoob/nanodegree/and/vegginner/ui/beginning/BeginningFragment.java
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/ui/beginning/BeginningFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.juancoob.nanodegree.and.vegginner.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -18,6 +21,9 @@ import butterknife.ButterKnife;
  * Created by Juan Antonio Cobos Obrero on 24/07/18.
  */
 public class BeginningFragment extends Fragment {
+
+    @BindView(R.id.tv_main_description)
+    public TextView mainDescriptionTextView;
 
     public BeginningFragment() {
         // Required empty public constructor
@@ -39,5 +45,11 @@ public class BeginningFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_beginning, container, false);
         ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mainDescriptionTextView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
