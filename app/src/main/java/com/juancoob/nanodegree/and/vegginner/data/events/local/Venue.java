d42890ca9799f7998cc49d3cd0b0192a532da153
juancoob <ja.cobos.obrero@gmail.com>
2018-09-29 21:03:12 +0200

    feat: Added the venue's model for the event's model

---
+++ app/src/main/java/com/juancoob/nanodegree/and/vegginner/data/events/local/Venue.java
@@ -0,0 +1,31 @@
+package com.juancoob.nanodegree.and.vegginner.data.events.local;
+
+/**
+ * This class is the venue's model for the event's model
+ *
+ * Created by Juan Antonio Cobos Obrero on 27/09/18.
+ */
+public class Venue {
+
+    //id
+    private int mId;
+
+    //localized_country_name
+    private String mCountry;
+
+    //city
+    private String mCity;
+
+    //address_1
+    private String mAddress;
+
+    //name
+    private String mPlaceName;
+
+    //lon
+    private Long mLongitude;
+
+    //lat
+    private Long mLatitude;
+
+}
